da448f507c760659a1d764b22dbbda6c29557b4a
PaulS <psoliz333@gmail.com>
2022-05-11 19:54:57 -0500

    created html for handlebars and got a visual site

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,1 +1,53 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Comment, Post, User } = require('../models')
 
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+module.exports = router;
+
+// router.get('/', (req, res) => {
+//     Post.findAll({
+//         attributes: [
+//             'id',
+//             'title',
+//             'content',
+//             'created_at'
+//         ],
+//         include: [
+//             {
+//                 model: Comment,
+//                 attributes: [
+//                     'id',
+//                     'comment_text',
+//                     'user_id',
+//                     'post_id'
+//                 ],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             },
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }
+//         ]
+//     })
+//         .then(dbPostData => {
+//             const post = dbPostData.map(post => post.get({ plain: true }));
+
+//             //here might lie the issue
+//             res.render('homepage', {
+//                 post,
+//             });
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         })
+// })
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 
-const apiRouters = require('./api')
+const apiRouters = require('./api');
+const homeRoutes = require('./homepage-routes');
 
 router.use('/api', apiRouters);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
