4be4f2b573daad5ef4c117505c2720cca68a8a39
PaulS <psoliz333@gmail.com>
2022-05-11 23:15:24 -0500

    created single post handlebars

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -2,52 +2,97 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Comment, Post, User } = require('../models')
 
-router.get('/', (req, res) => {
-    res.render('homepage');
-});
-
 module.exports = router;
 
-// router.get('/', (req, res) => {
-//     Post.findAll({
-//         attributes: [
-//             'id',
-//             'title',
-//             'content',
-//             'created_at'
-//         ],
-//         include: [
-//             {
-//                 model: Comment,
-//                 attributes: [
-//                     'id',
-//                     'comment_text',
-//                     'user_id',
-//                     'post_id'
-//                 ],
-//                 include: {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             },
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//         .then(dbPostData => {
-//             const post = dbPostData.map(post => post.get({ plain: true }));
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'user_id',
+                    'post_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const post = dbPostData.map(post => post.get({ plain: true }));
 
-//             //here might lie the issue
-//             res.render('homepage', {
-//                 post,
-//             });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         })
-// })
+            //how to preview a single post
+            // res.render('homepage', dbPostData[0].get({ plain: true }));
+
+            res.render('homepage', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'user_id',
+                    'post_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id ' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+
+            res.render('single-post', { post });
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers')
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({})
+const hbs = exphbs.create({ helpers })
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
